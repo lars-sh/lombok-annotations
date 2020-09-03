07e94f455c4309eeb44ed1624f2f3add2aec6b83
Lars Knickrehm <mail@lars-sh.de>
2020-09-04 01:25:56 +0200

    Improve Travis suppressions for JDK11 and JDK14

--- travis-suppressions.sh
+++ travis-suppressions.sh
@@ -2,7 +2,18 @@
 function suppressJdk11VarIsNotAnAllowedAnnotationName() {
 	if [ ${JDK_VERSION} -ge 11 ]; then
 		cat < /dev/stdin \
+		| grep --invert-match --perl-regexp "^\\[ERROR\\] Error while creating javadoc report: $"
 		| grep --invert-match --perl-regexp "^\\[ERROR\\] MavenReportException: Error while generating Javadoc: $"
+	else
+		cat < /dev/stdin
+	fi
+}
+
+# JDK14: The JavaDoc creation logs errors about missing "javadoc-bundle-option" folders.
+function suppressJdk14Warnings() {
+	if [ ${JDK_VERSION} -eq 14 ]; then
+		cat < /dev/stdin \
+		| grep --invert-match --perl-regexp "^\\[ERROR\\] Error fetching link: .*[/\\\\]target[/\\\\]javadoc-bundle-options\\. Ignored it\\.$"
 	else
 		cat < /dev/stdin
 	fi
@@ -11,4 +22,5 @@ function suppressJdk11VarIsNotAnAllowedAnnotationName() {
 JDK_VERSION=`echo ${TRAVIS_JDK_VERSION} | grep --only-matching --perl-regexp \\\\d+`
 
 cat < /dev/stdin \
-| suppressJdk11VarIsNotAnAllowedAnnotationName
+| suppressJdk11VarIsNotAnAllowedAnnotationName \
+| suppressJdk14Warnings
