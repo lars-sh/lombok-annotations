14002b4537873d7021857c3fd72dc19477680b5d
lars-sh <lars-sh@users.noreply.github.com>
2025-11-10 17:43:18 +0000

    deploy: fe7522e25948c787cfb6ea27f212ace352d9b3aa

--- apidocs/javadoc.sh
+++ apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Adopt_jdk/8.0.462-8/x64/jre/../bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
+/opt/hostedtoolcache/Java_Adopt_jdk/8.0.472-8/x64/jre/../bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
